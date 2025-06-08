eaf26cf913cd93c98254d314d1a7393775ed4a5a
stallone odhiambo <odhiambostallone73@gmail.com>
2025-06-08 07:54:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/AppProviders.tsx
+++ src/components/layout/AppProviders.tsx
@@ -4,6 +4,7 @@
 import { ExpenseProvider, BudgetProviderActual } from "@/contexts/ExpenseContext";
 import { AuthProvider } from "@/contexts/AuthContext"; // Import AuthProvider
 import { SettingsProvider } from "@/contexts/SettingsContext"; // Import SettingsProvider
+import { SavingsGoalProvider } from "@/contexts/SavingsGoalContext"; // Import SavingsGoalProvider
 import type { ReactNode } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
@@ -12,13 +13,15 @@ export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <AuthProvider> {/* AuthProvider should be high up */}
       <SettingsProvider> {/* SettingsProvider wraps content needing settings */}
-        <TooltipProvider delayDuration={0}>
-          <ExpenseProvider>
-            <BudgetProviderActual>
-              {children}
-            </BudgetProviderActual>
-          </ExpenseProvider>
-        </TooltipProvider>
+        <ExpenseProvider>
+          <BudgetProviderActual>
+            <SavingsGoalProvider> {/* Add SavingsGoalProvider here */}
+              <TooltipProvider delayDuration={0}>
+                {children}
+              </TooltipProvider>
+            </SavingsGoalProvider>
+          </BudgetProviderActual>
+        </ExpenseProvider>
       </SettingsProvider>
     </AuthProvider>
   );
